0448dae45d33eeef1abeaca47eae28342b343138
Dimitri WALTER <dimitri.walter@ens.uvsq.fr>
2022-12-12 16:22:56 +0100

    Non

--- traitementdetexte.py
+++ traitementdetexte.py
@@ -4,16 +4,17 @@
     
 
     
-    
-
-
-
-        
-        
-
 def Lecture(emplacement):
     fichier = open(emplacement,"r")
     return(fichier.read())
 
 
 print(Lecture("D:\Projet\test.txt"))
+
+def transform(char):
+    for elmnt in char :
+        if elmnt 
+        
+
+def Lecture(emplacement):
+    fichier = open(emplacement)
