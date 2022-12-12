b1eabcded014c63a433d0c64ccc714b5efac0bee
Adrien <adrien.carbonel@ens.uvsq.fr>
2022-12-12 16:35:10 +0100

    Modif

--- traitementdetexte.py
+++ traitementdetexte.py
@@ -3,18 +3,3 @@
 
     
 
-    
-def Lecture(emplacement):
-    fichier = open(emplacement,"r")
-    return(fichier.read())
-
-
-print(Lecture("D:\Projet\test.txt"))
-
-def transform(char):
-    for elmnt in char :
-        if elmnt in char:
-        
-
-def Lecture(emplacement):
-    fichier = open(emplacement)
